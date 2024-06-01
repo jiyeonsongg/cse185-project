86184643fef15db892377b2722be479d73bb16d9
Jiyeon Song <73157135+jiyeonsongg@users.noreply.github.com>
2024-05-31 21:39:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ setup(
     name='peak_a_view',
     version='0.1.0',
     description='CSE185 DEMO PROJECT',
-    author='Jiyeon Song, Ivana Roque, Jacob Ketchum',
-    author_email='jis036@ucsd.edu, iroque@ucsd.edu, dketchum@ucsd.edu',
+    author='Jiyeon, Jacob, Ivana',
+    author_email='jis036@ucsd.edu, dketchum@ucsd.edu',
     packages=find_packages(),
     install_requires=[
         'pandas',
         'pysam',
         'macs2',
     ],
-    scripts=['peak_a_view/peak_a_view_tool.py'],
     entry_points={
         'console_scripts': [
             'peak_a_view=peak_a_view.peak_a_view:main'
