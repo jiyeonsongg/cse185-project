6138fcad91d6b104ccf0a703dab070addbbef115
Jiyeon Song <73157135+jiyeonsongg@users.noreply.github.com>
2024-05-31 21:16:04 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup, find_packages
-#from peakfind.__version__ import __version__
-# peakfind version to import
+
 setup(
-  name = 'peak_a_view',
-  version='0.1.0',
-  description = 'CSE185 DEMO PROJECT',
-  author = 'Jiyeon, Jacob, Ivana',
-  author_email = 'jis036@ucsd.edu, dketchum@ucsd.edu',
-  packages = find_packages(),
-  install_requires=[ # make sure to list down all
-      'macs2',
+    name='peak_a_view',
+    version='0.1.0',
+    description='CSE185 DEMO PROJECT',
+    author='Jiyeon Song, Ivana Roque, Jacob Ketchum',
+    author_email='jis036@ucsd.edu, iroque@ucsd.edu, dketchum@ucsd.edu',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'pysam',
+        'macs2',
     ],
-  scripts=['scripts/script'],
-  entry_points = {
-    "console_scripts": [
-      "peak_a_view = peak_a_view.peak_a_view:main"
-    ],
-  },
+    scripts=['peak_a_view/peak_a_view_tool.py'],
+    entry_points={
+        'console_scripts': [
+            'peak_a_view=peak_a_view.peak_a_view:main'
+        ],
+    },
 )
